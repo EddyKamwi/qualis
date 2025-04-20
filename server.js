6528270e8fd048a963eec7ce993f4bd51ec6c557
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-21 00:41:34 +0200

    add node

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+require("dotenv").config()
+const path = require("path")
+const index = path.join(__dirname,"./index.html")
+const app = express()
+
+app.use(express.static("./"))
+app.use("/", (req,res) => {
+    res.sendFile(index)
+})
+app.listen(process.env.PORT, () => {
+    console.log("app listening at http://localhost:"+process.env.PORT)
+})
